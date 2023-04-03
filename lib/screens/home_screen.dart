a7792112ab44f6d86d3acf72bff2a0d81ff5641e
lucaslpublio <62101230+lucaslpublio@users.noreply.github.com>
2023-04-03 17:35:30 -0300

    Add files via upload

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,105 @@
+import 'package:bottom_navigation_bar/utilities/icon_path_util.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(30),
+        color: Colors.white,
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              width: 190,
+              height: 190,
+              child: Image.asset("assets/icons/logo_profile.jpg"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              // autofocus: true,
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                labelText: "Nome Completo",
+                labelStyle: TextStyle(
+                  color: Colors.black38,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20,
+                ),
+              ),
+              style: TextStyle(fontSize: 20),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                labelText: "Email",
+                labelStyle: TextStyle(
+                  color: Colors.black38,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20,
+                ),
+              ),
+              style: const TextStyle(
+                  fontSize: 20
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              // autofocus: true,
+              keyboardType: TextInputType.text,
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: "Senha",
+                labelStyle: TextStyle(
+                  color: Colors.black38,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20,
+                ),
+              ),
+              style: const TextStyle(fontSize: 20),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              width: double.infinity,
+              height: 60,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: MaterialButton(
+                onPressed: () => print("Successul Login."),
+                color: Colors.lightGreen,
+                child: Text(
+                  'CADASTRAR',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Divider(
+              color: Colors.black,
+              height: 30,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
